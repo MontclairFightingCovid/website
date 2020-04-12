796835ff8af0dc3d68a215337ecf4dee3f6fd878
aftercandey <62780543+aftercandey@users.noreply.github.com>
2020-04-12 10:45:00 -0400

    hello evryone!!!

--- src/components/seo.js
+++ src/components/seo.js
@@ -68,7 +68,9 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+    >
+      <script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us19.list-manage.com","uuid":"27a998c11f0a786810ef8a960","lid":"42668ee870","uniqueMethods":true}) })</script>
+    <Helmet>
   )
 }
 
